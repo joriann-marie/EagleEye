2ba5e4af0b0d66e2501daba0135478605c458995
SpencerPetty <spencerpetty@utexas.edu>
2022-04-28 13:42:45 -0500

    EpochClockSwapper is in working condition and commented

--- EpochClockSwapper.java
+++ EpochClockSwapper.java
@@ -1,32 +1,51 @@
 import java.util.Date;
 
+/*
+*  Class for easing the conversion of UNIX epoch time. Intended for use with begin/end of day and 
+*  for specifying up to when connections in the flow log should be processed
+*
+*/
 public class EpochClockSwapper {
     
-    private static final long TRUNCATE_DATE = 86_400; // UNIX time will only give clock time
+    private static final long TRUNCATE_DATE = 86_400; // Shave off MM/dd/yyyy and only return clock time
+    private static final int OFFSET = 3_600; // Don't know why but having this perfectly aligns truncated time to give just the hour and minute
     
-    private Date date;
+    private Date date; // Main Date object
     
-    private String dateTruncatedTime = null;
-
+    // Takes current local time in milliseconds
     public EpochClockSwapper (long UNIXTime) {
         date = new Date(UNIXTime);
     }
     
+    /*
+    * Returns the time as a long epoch
+    */
     public long getUNIXTime() {
         return date.getTime();
     }
     
+    /*
+    * Returns the time in MM/dd/yyyy HH:mm:ss format
+    */
     public String getDateTime() {
             // Taken from https://www.epochconverter.com/
             return new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (getUNIXTime()));
     }
     
+    /*
+    * Returns the time as a long epoch, but as if it were on day 0 so as to only have the clock time
+    */
     public long getUNIXTruncatedTime() {
-        return getUNIXTime() % TRUNCATE_DATE;
+        return (getUNIXTime()/1000) % TRUNCATE_DATE + OFFSET;
     }
     
+    /*
+    * Returns the time in HH:mm:ss format
+    */
     public String getTruncatedDateTime() {
             // Taken from https://www.epochconverter.com/
-            return new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (getUNIXTruncatedTime()));
+            final int TRUNCATE_CALENDAR_DATE = 11;
+            String result = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (getUNIXTime()));
+            return result.substring(TRUNCATE_CALENDAR_DATE);
     }
 }
